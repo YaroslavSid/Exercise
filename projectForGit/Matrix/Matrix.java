d88ccd09b3e99834d23d9fe63cac64cadc7d89a6
Yaroslav <yaroslavsid05@gmail.ru>
2021-01-08 23:51:28 +0200

    Revert "Change Matrix"
    This reverts commit f06059f69f020710c5eefd81c2c544e060e66328.
    
    Revert occasionally removed commit

--- projectForGit/Matrix/Matrix.java
+++ projectForGit/Matrix/Matrix.java
@@ -1,52 +1,24 @@
 package Matrix;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Matrix {
     public static void main(String[] args) {
         MatrixOne matrixOne = new MatrixOne();
-        Random random = new Random();
+        int[][] A = {{1, 1}, {2, 8}};//1 1      14 1
+        int[][] B = {{14, 1}, {2, 0}};//2 8     2  0
+        matrixOne.setA(A,B);
 
-
-        int[][] firstMatrix = new int[3][3];
-        for (int i = 0; i < firstMatrix.length; i++) {
-            for (int j = 0; j < firstMatrix[i].length; j++) {
-
-                firstMatrix[i][j] = random.nextInt(19);
-            }
-        }
-        System.out.println("First matrix =  " + Arrays.deepToString(firstMatrix) + " ");
-        System.out.println();
-// --------------------------------------------------------------------------------------
-
-        int[][] secondMatrix = new int[3][3];
-        for (int i = 0; i < secondMatrix.length; i++) {
-            for (int j = 0; j < secondMatrix[i].length; j++) {
-                secondMatrix[i][j] = random.nextInt(19);
-
-            }
-        }
-        System.out.println("Second matrix =  " + Arrays.deepToString(secondMatrix) + " ");
-        System.out.println();
-        System.out.println("Sum all  matrix = " + Arrays.deepToString(matrixOne.sumMatrix(firstMatrix, secondMatrix)) + "\n");
     }
 }
 
 class MatrixOne {
-
-    final int[][] matrix1 = new int[3][3];
-
-    public int[][] sumMatrix(int[][] first, int[][] second) {
-        for (int i = 0; i < first.length; i++) {
-            for (int j = 0; j < first[i].length; j++) {
-                try {
-                    matrix1[i][j] = first[i][j] + second[i][j];
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    e.printStackTrace();
-                }
+    int  Matrix1;
+    public void   setA(int[][] A , int[][] B) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                Matrix1 = A[i][j] + B[i][j];
+                System.out.print(Matrix1 + " ");
             }
+            System.out.println();
         }
-        return matrix1;
     }
 }
